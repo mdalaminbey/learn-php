e00966be3c1ea080aef1f8ca42588d5ff3b5bad1
MD AL AMIN <mdalaminbey@gmail.com>
2023-01-22 23:09:10 +0600

    Update index.php

--- crud/index.php
+++ crud/index.php
@@ -1,7 +1,25 @@
-<?php include_once './header.php' ; ?>
+<?php include_once './header.php'; ?>
 
-<form action="" method="post">
+<?php
 
-</form>
+if(!empty($_POST['name']) && !empty($_POST['email'])) {	
+	var_dump($_POST);
+}
+?>
+<div class="container">
+	<div class="row">
+		<form method="post" target="">
+			<div class="mb-3">
+				<label class="form-label">Full Name</label>
+				<input type="text" name="name" class="form-control">
+			</div>
+			<div class="mb-3">
+				<label class="form-label">Email address</label>
+				<input type="email" name="email" class="form-control">
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+</div>
 
-<?php include_once get_root_dir('/crud/footer.php') ;?>
+<?php include_once get_root_dir('/crud/footer.php'); ?>
